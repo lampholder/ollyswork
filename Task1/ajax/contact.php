5e106bf97b67a927ada15eb070ced53932560258
root <root@localhost.localdomain>
2016-12-10 08:12:38 -0500

    A simple form submission thing mocking up accepting a request.

--- Task1/ajax/contact.php
+++ Task1/ajax/contact.php
@@ -1,5 +1,13 @@
-<?php
-if (isset($_POST['firstName'], $_POST['lastName'], $_POST['DOB'], $_POST['email'])) {
-	print_r($_POST);
+<?PHP
+header('Content-Type: application/json');
+
+if (!empty($_POST['firstName']) and !empty($_POST['lastName']) and !empty($_POST['DOB']) and !empty($_POST['email'])) {
+    echo json_encode(["success" => True,
+                      "message" => "Thanks for subscribing!"]);
 }
+else {
+    echo json_encode(["success" => False,
+                      "error" => "Insufficient data submitted."]);
+}
+
 ?>
