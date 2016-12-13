a660be3cdd4b0874c3112b030f6237aeee79a234
root <lampholder@gmail.com>
2016-12-13 18:55:56 -0500

    made local storage work

--- Task1/ajax/contact.php
+++ Task1/ajax/contact.php
@@ -1,13 +1,13 @@
 <?PHP
-header('Content-Type: application/json');
-
-if (!empty($_POST['firstName']) and !empty($_POST['lastName']) and !empty($_POST['DOB']) and !empty($_POST['email'])) {
-    echo json_encode(["success" => True,
-                      "message" => "Thanks for subscribing!"]);
+ header('Content-Type: application/json');
+ 
+ if (!empty($_POST['firstName']) and !empty($_POST['lastName']) and !empty($_POST['DOB']) and !empty($_POST['email'])) {
+     echo json_encode(["success" => True,
+                       "message" => "Thanks for subscribing!"]);
+  }
+ else {
+     echo json_encode(["success" => False,
+                       "error" => "Insufficient data submitted."]);
 }
-else {
-    echo json_encode(["success" => False,
-                      "error" => "Insufficient data submitted."]);
-}
-
+ 
 ?>
--- Task1/js/fontSize.js
+++ Task1/js/fontSize.js
@@ -1,16 +1,13 @@
 $("#fontPlusBtn").click(function (){
     $("#textDiv > *").css("font-size", function(i, value) {
+	window.localStorage.setItem('fontSize', parseInt(value) + 1.1);
         return parseInt(value) + 1.1;
     });
 });
 $("#fontMinusBtn").click(function (){
     $("#textDiv > *").css("font-size", function(i, value) {
+	window.localStorage.setItem('fontSize', parseInt(value) - 1.1);
         return parseInt(value) - 1.1;
     });
 });
-    $(".colorBlock").click(function() {
-      //Get the background color of the clicked color block by using the this object
-      var $backgroundColor = $(this).css("background-color");
-      //Set the body's background-color to the received background-color
-      $("body").css("background-color", $backgroundColor);
-    });
+
