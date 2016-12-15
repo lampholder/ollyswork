6e630f5657eff8d7167ee766d310c503a15e8d69
root <lampholder@gmail.com>
2016-12-15 17:35:25 -0500

    should have been committed earlier

--- Task1/js/stockcall.js
+++ Task1/js/stockcall.js
@@ -4,7 +4,7 @@ var stock = "SKX";
 $.ajax({url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + stock + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=", 
         dataType: "json",
         success: function(result){
-                         $('#bid').text(result.query.results.quote.Bid);
+                         $('#bid').text(result.query.results.quote.DaysHigh);
         }
 });
 });
